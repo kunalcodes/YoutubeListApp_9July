4a04025270b715a25ddea8d5599022633afc610a
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-10-21 16:48:57 +0530

    debugging comment added to activity

--- app/src/main/java/com/example/youtube_list_app/ItemClickListener.java
+++ app/src/main/java/com/example/youtube_list_app/ItemClickListener.java
@@ -1,5 +1,11 @@
 package com.example.youtube_list_app;
 
+
+
 public interface ItemClickListener {
+
+    /*this method is called from the view holder when item is clicked
+    and implemented in the activity
+     */
     void onItemClicked(int position, String s);
 }
